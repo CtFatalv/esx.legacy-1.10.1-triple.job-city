93b51a7579a78f30e2e6c1448a1326090973d9b1
CtFatalv <contact.fatalv@gmail.com>
2023-08-26 11:40:21 +0200

    Legacy City

--- resources/[ajout]/outils/client/stopaction.lua
+++
@@ -1,9 +0,0 @@
--- bouton x annuler une animation
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(1)
-		if IsControlJustPressed(0, 73) then
-			ClearPedTasks(GetPlayerPed(-1))
-		end
-	end
-end)
--- resources/[ajout]/outils/server/sirene.lua
+++
@@ -1,4 +0,0 @@
-RegisterServerEvent("siren:sync")
-AddEventHandler("siren:sync", function(state, veh)
-    TriggerClientEvent("siren:ClientSync", -1, state, veh)
-end)
